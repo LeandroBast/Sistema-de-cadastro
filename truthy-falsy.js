8ffc83ab5b4592c93d4f121a6bb86014b0811461
unknown <leandroloureco@gmail.com>
2022-09-16 11:20:41 -0300

    novas adicoes

---
+++ truthy-falsy.js
@@ -0,0 +1,13 @@
+//const usuariologado = true
+//const contapaga = false
+
+//console.log (0 == false)
+//console.log (""== false)
+//console.log (1 == true)
+
+
+let minhaVar;
+let varNull = null;
+console.log(minhaVar)
+console.log(varNull)
+
